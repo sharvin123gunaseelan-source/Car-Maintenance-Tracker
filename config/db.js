e7be6e4c60725f94c151eb386ae51c9139aaa803
Sharvin <sharvin123gunaseelan@gmail.com>
2026-07-04 00:35:05 +0800

    final project ready

--- config/db.js
+++ config/db.js
@@ -1,12 +1,7 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+const db = mysql.createConnection(process.env.MYSQL_URL);
 
 db.connect((err) => {
   if (err) {
